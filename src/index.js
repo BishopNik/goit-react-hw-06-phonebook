d9531107774b90c7df18f23562b139660eee2bc0
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-10-09 18:26:51 +0200

    redux-persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/Helpers/GlobalStyle';
 import { store } from './components/Redux/store';
-import { Provider } from 'react-redux';
+import { persistor } from './components/Redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<ThemeProvider theme={{}}>
-				<App />
-				<GlobalStyle />
-			</ThemeProvider>
+			<PersistGate loading={null} persistor={persistor}>
+				<ThemeProvider theme={{}}>
+					<App />
+					<GlobalStyle />
+				</ThemeProvider>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
